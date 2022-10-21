447b7934bdf17e7ce036c5ca932f329e65303c63
Hossein Tajfirouz <hosseintajfirouz@gmail.com>
2022-10-21 14:46:03 +0400

    extra comments added

--- TaskManagement/urls.py
+++ TaskManagement/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
+
     path('accounts/', include('accounts.urls')),
     path('profile/', include('user_profiles.urls')),
     path('api/', include('tasks.urls')),
